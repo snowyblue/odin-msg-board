4b942420029306e536e1cc1576964d8aba2c89e6
Cody Tope <codytope@gmail.com>
2023-01-27 06:00:06 -0500

    Express-Handlebars functionality implemented

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const exphbs = require('express-handlebars')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,8 +11,12 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'index'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('../views/main');
 });
 
 module.exports = router;
