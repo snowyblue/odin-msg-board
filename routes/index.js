616b56bd34c0d54c6a90a01022b504bafaaa622b
Cody Tope <codytope@gmail.com>
2023-01-29 06:59:19 -0500

    Added database functionality, problems with hbs displaying data

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const password = 'NenEENpbSQFzUiF6';
+const url = `mongodb+srv://general_user:${password}@cluster0.nbp0qcd.mongodb.net/odinposts?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false);
+
+const postSchema = new mongoose.Schema({
+  text: String,
+  user: String,
+  added: Date,
+})
+
+const Post = mongoose.model('posts',postSchema);
 
 /* GET home page. */
-router.get(['/', '/home'], function(req, res, next) {
-  res.render('../views/main', { title: "Mini Message Board", messages: messages });
+router.get(['/','/home'], function(req, res, next) {
+  mongoose.connect(url);
+  Post.find({}).then(results => {
+    const messages = results;
+    res.render('../views/main', { title: "Mini Message Board", messages: messages });
+  })
 });
 
-// router.post('/newpost', function(req, res) {
-//   const { name, message }  = req.body
-//   messages.push({text: message, user: name, added: currentDate});
-//   res.redirect("/");
-// })
+router.post('/newpost', function(req, res) {
+  const { user, text }  = req.body;
+  
+  mongoose.connect(url);
+  const newPost = new Post({
+    text: text,
+    user: user,
+    added: new Date(),
+  })
 
-// router.get('/newpost', (req, res, next) => {
-//   res.render('../views/form');
-// })
+  newPost.save().then(result => {
+    mongoose.connection.close();
+    res.redirect('/');
+  })
+})
 
-
-
-module.exports = {indexRouter: router, messages: messages}
+module.exports = router;
