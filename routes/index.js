b33be48eb1b3c0b1ff9f19d15c8f1ff6750f974c
Cody Tope <codytope@gmail.com>
2023-01-29 07:48:49 -0500

    Fixed issue w/ handlebars not displaying fetched data

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const password = 'NenEENpbSQFzUiF6';
 const url = `mongodb+srv://general_user:${password}@cluster0.nbp0qcd.mongodb.net/odinposts?retryWrites=true&w=majority`
 mongoose.set('strictQuery',false);
+mongoose.connect(url);
 
 const postSchema = new mongoose.Schema({
   text: String,
@@ -15,9 +16,12 @@ const Post = mongoose.model('posts',postSchema);
 
 /* GET home page. */
 router.get(['/','/home'], function(req, res, next) {
-  mongoose.connect(url);
-  Post.find({}).then(results => {
-    const messages = results;
+
+  Post.find({},{_id:0, __v:0}).then(results => {
+    const messages = results.map(result => {
+      return {text: result.text, user: result.user, added: result.added}
+    });
+    console.log(messages);
     res.render('../views/main', { title: "Mini Message Board", messages: messages });
   })
 });
@@ -26,7 +30,6 @@ router.get(['/','/home'], function(req, res, next) {
 router.post('/newpost', function(req, res) {
   const { user, text }  = req.body;
   
-  mongoose.connect(url);
   const newPost = new Post({
     text: text,
     user: user,
@@ -34,7 +37,6 @@ router.post('/newpost', function(req, res) {
   })
 
   newPost.save().then(result => {
-    mongoose.connection.close();
     res.redirect('/');
   })
 })
